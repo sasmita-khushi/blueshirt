7832cd348b2b1ee3f0e5c51667029a3e3f25ee3f
Sasmita Dehury <sasmitadehury.khushboo@gmail.com>
2024-07-18 16:43:04 +0530

    my 3rd commit

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,4 @@
 declare module "blueshirt" {
   export function getColor(): string;
+  export function addTwoNumber(a: number, b: number): number;
 }
--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 export function getColor() {
   return "#0000FF";
 }
+
+export function addTwoNumber(a, b) {
+  return a + b;
+}
